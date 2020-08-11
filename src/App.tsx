fdee2613edb64ac3461bfcfe77a6e8b354bd5c77
artyhacker <artyhacker@outlook.com>
2020-08-11 16:54:28 +0800

    feat: 添加Nav，优化整体布局使填充满页面

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Container from '@material-ui/core/Container';
 import AddPage from './pages/AddPage';
+import styles from './App.module.css';
+import { Paper, Box } from '@material-ui/core';
+import Nav from './components/BottomNav';
 
 export default function AppContainer() {
+  const vh = useMemo(() => window.innerHeight, []);
+
   return (
     <React.Fragment>
       <CssBaseline />
       <Container maxWidth="md">
-        <AddPage />
+        <Paper className={styles.container} style={{ height: vh }}>
+          <Box className={styles.page}>
+            <AddPage />
+          </Box>
+          <Nav />
+        </Paper>
       </Container>
     </React.Fragment>
   );
